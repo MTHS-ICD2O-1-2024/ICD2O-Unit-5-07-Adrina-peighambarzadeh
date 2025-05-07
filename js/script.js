50eab21528572c68553d43dfa29c5984b0114810
Adrina-peighambarzadeh <Adrina.peighambarzadeh@stu.ocsb.ca>
2025-05-07 13:42:11 +0000

    init

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Adrina Peighambarzadeh All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Adrina Peighambarzadeh
+// Created on: May 2025
 // This file contains the JS functions for index.html
+
+/**
+ * This function calculates the sum of all numbers from 1 to the given number.
+ */
+// eslint-disable-next-line no-unused-vars
+function doMultiplication() {
+  // input
+  let counter = 1
+  let answer = 0
+  const userNumber = parseInt(document.getElementById('user-number').value)
+
+  // process using loop
+  while (counter <= userNumber) {
+    answer = answer + counter
+    counter = counter + 1
+  }
+
+  // process using formula
+  const formulaAnswer = (userNumber * (userNumber + 1)) / 2
+
+  // output
+  document.getElementById('answer').innerHTML =
+    'Sum using loop: ' + answer + '<br>' +
+    'Sum using formula: ' + formulaAnswer
+}
